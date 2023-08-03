81d605c399fafb41525754412fe0e66361f4bf57
LakeshiaJ <joseph.mayaka@yahoo.com>
2023-08-03 12:41:56 +0000

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstLine = 'c is fun';
+const secondLine = 'phython is cool';
+const thirdline = 'javascript is amazing';
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
