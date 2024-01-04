7a72e2f7a2c098c9bbfb74b398a7031ca02559e2
joseph <joseph.mayaka@yahoo.com>
2024-01-04 13:42:50 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    # Print sorted name from directory
-    for name in sorted(dir(hidden_4)):
-        # print only names that do not start with __
-        if name[:2] != '__':
-            print("{}".format(name))
-
+    temp = dir(hidden_4)
+    for i in range(len(temp)):
+        for j in range(len(temp[i])):
+            if (temp[i][j] == '_' and temp[i][j+1] == '_'):
+                break
+            else:
+                print(temp[i])
+                break
